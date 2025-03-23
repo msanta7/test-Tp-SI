5d985bb2b206bde9a2f4df199d8d886e026635a5
Goudjil Yacine Abdelmadjid <goudjil.yacine.abdelmadjid@gmail.com>
2025-03-23 17:49:29 +0000

    Update FonctionnalitesReser.java

--- testTp/src/testTp/FonctionnalitesReser.java
+++ testTp/src/testTp/FonctionnalitesReser.java
@@ -6,84 +6,63 @@ import java.util.List;
 
 public class FonctionnalitesReser {
 	
-	List <Reservation> reserv; 
+	List<Reservation> reserv; 
 	
 	public FonctionnalitesReser() {
 		reserv = new ArrayList<>();
     }
 	
-	
-	public void ajouterRes(int id, String nomEmp, int dureeReservation, LocalDateTime debut)
-	{
-		Reservation res=new Reservation(id,nomEmp,dureeReservation,debut);
+	public void ajouterRes(int id, String nomEmp, int dureeReservation, LocalDateTime debut,String salle) {
+		Reservation res = new Reservation(id, nomEmp, dureeReservation, debut,salle);
 		reserv.add(res);
 	}
 	
-	public void afficherRes()
-	{
-		if(!reserv.isEmpty())
-		{
-			for(Reservation res:reserv)
-			{
-				System.out.println("la liste des reservation : ");
+	public void afficherRes() {
+		if (!reserv.isEmpty()) {
+			System.out.println("la liste des reservation : ");
+			for (Reservation res : reserv) {
 				System.out.println(res);
 			}
+		} else {
+			System.out.println("aucune reservation");
 		}
-		else {
-		        System.out.println("aucune reservation");
-		}
-		
 	}
 	
-	public void suppRes(Reservation reservation)throws ReservationNonTrouve
-	{
-		if(reserv.contains(reservation))
-		{
+	public void suppRes(Reservation reservation) throws ReservationNonTrouve {
+		if (reserv.contains(reservation)) {
 			reserv.remove(reservation);
+		} else {
+			throw new ReservationNonTrouve("reservation introuvable");
 		}
-			else {
-				throw new ReservationNonTrouve("reservation introuvable");
-			}
-		}
+	}
 	
-	
-	
-	public void modifierRes(int id,String nomEmp, int dureeReservation, LocalDateTime debut) throws ReservationNonTrouve
-	{
-		for(Reservation res:reserv)
-		{
-			if(res.getId()==id)
-			{
+	public void modifierRes(int id, String nomEmp, int dureeReservation, LocalDateTime debut) throws ReservationNonTrouve {
+		boolean found = false;
+		for (Reservation res : reserv) {
+			if (res.getId() == id) {
 				res.setDebut(debut);
 				res.setDureeReservation(dureeReservation);
 				res.setNomEmp(nomEmp);
+				found = true;
 				break;
 			}
-			else {
-				throw new ReservationNonTrouve("reservation introuvable");
-			}
+		}
+		if (!found) {
+			throw new ReservationNonTrouve("reservation introuvable");
 		}
 	}
 	
-	public void doubleReservCheck(int id)
-	{
-		int count=0;
-		for(Reservation res:reserv)
-		{
-			if(res.getId()==id)
-			{
+	public void doubleReservCheck(int id) {
+		int count = 0;
+		for (Reservation res : reserv) {
+			if (res.getId() == id) {
 				count++;
 			}
 		}
-		if(count>1)
-		{
-			System.out.println("il y a "+count+"reservation");
-		}
-		else if(count==1)
-		{
+		if (count > 1) {
+			System.out.println("il y a " + count + " reservation");
+		} else if (count == 1) {
 			System.out.println("pas de double reservation");
 		}
 	}
-	
-
 }
