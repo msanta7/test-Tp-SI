52b91bd486552e4119cc587178605207800a9ab6
Goudjil Yacine Abdelmadjid <goudjil.yacine.abdelmadjid@gmail.com>
2025-03-23 17:48:55 +0000

    Update Main.java

--- testTp/src/testTp/Main.java
+++ testTp/src/testTp/Main.java
@@ -1,10 +1,15 @@
 package testTp;
 
+import java.time.LocalDateTime;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        Salle salle1 = new Salle("B", 1);
+        System.out.println(salle1);
 
-	}
-
+        FonctionnalitesReser fonctionnalitesReser = new FonctionnalitesReser();
+        fonctionnalitesReser.ajouterRes(1, "Yacine ", 2, LocalDateTime.now(), salle1.toString());
+        fonctionnalitesReser.afficherRes();
+    }
 }
